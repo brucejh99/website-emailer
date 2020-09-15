95bf3ff3eec9115c42195f30e50fa7019681a0c6
brucejh99 <bjhong@yahoo.com>
2020-09-15 19:20:53 -0400

    set up server with oak

--- index.ts
+++ index.ts
@@ -1,12 +1,28 @@
-import { serve } from 'https://deno.land/std@0.69.0/http/server.ts';
+import { Application } from "https://deno.land/x/oak/mod.ts";
 import { parse } from 'https://deno.land/std/flags/mod.ts';
+import { SmtpClient } from 'https://deno.land/x/smtp/mod.ts';
+import 'https://deno.land/x/dotenv/load.ts';
 
 const { args } = Deno;
 const DEFAULT_PORT = 8000;
 const argPort = parse(args).port;
 
-const s = serve({ port: argPort ? Number(argPort) : DEFAULT_PORT });
+const client = new SmtpClient();
+const { SEND_EMAIL, PWD, RECV_EMAIL } = Deno.env.toObject();
+const connectConfig: any = {
+    hostname: "smtp.gmail.com",
+    port: 465,
+    username: SEND_EMAIL,
+    password: PWD,
+};
 
-for await (const req of s) {
-    req.respond({ body: 'Hello world' });
-}
+const app = new Application();
+
+// send email
+app.use(async ctx => {
+    const reqBody = ctx.request.body({ type: 'json' });
+    const { name, email, message } = await reqBody.value;
+    console.log(name, email, message);
+});
+
+await app.listen({ port: argPort ? Number(argPort) : DEFAULT_PORT });
