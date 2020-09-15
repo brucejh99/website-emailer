783ba045fdde46d7d2c32125ef07661cf9a877db
brucejh99 <bjhong@yahoo.com>
2020-09-15 19:30:36 -0400

    Updated for oak part 2

--- index.ts
+++ index.ts
@@ -1,19 +1,28 @@
-import { Application } from "https://deno.land/x/oak/mod.ts";
-import { parse } from 'https://deno.land/std/flags/mod.ts';
+import { Application, Router } from "https://deno.land/x/oak/mod.ts";
+import * as flags from 'https://deno.land/std/flags/mod.ts';
 import { SmtpClient } from 'https://deno.land/x/smtp/mod.ts';
 import 'https://deno.land/x/dotenv/load.ts';
 
-const { args } = Deno;
+const { args, exit } = Deno;
 const DEFAULT_PORT = 8000;
-const argPort = parse(args).port;
+const argPort = flags.parse(args).port;
+const port = argPort ? Number(argPort) : DEFAULT_PORT;
+if (isNaN(port)){
+    console.log("This is not port number");
+    exit(1);
+};
 
 const app = new Application();
+const router = new Router();
 
-// send email
-app.use(async ctx => {
+router.get('/', async ctx => {
     const reqBody = ctx.request.body({ type: 'json' });
     const { name, email, message } = await reqBody.value;
     console.log(name, email, message);
+    ctx.response.body = 'Success!'; 
 });
 
-await app.listen({ port: argPort ? Number(argPort) : DEFAULT_PORT });
+app.use(router.routes());
+app.use(router.allowedMethods());
+
+await app.listen({ port });
